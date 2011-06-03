3698887473c31c25e302292ba203b4f69d3e0d0e
Ghislain Seguin <ghislain@jivesoftware.com>
2011-06-03 13:30:44 -0700

    Fixed whitespaces

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,10 +4,10 @@ echo "Running pre-commit"
 
 # helper to print information for users on bad exit status
 function on_error {
-    if test $? -gt 0  ; then
-	echo "[error] $1"
-	exit 0
-    fi
+	if test $? -gt 0  ; then
+		echo "[error] $1"
+		exit 0
+	fi
 }
 
 # check connection
@@ -26,28 +26,28 @@ options_file=change-to-match-your-options-file
 # grabs all the names of the files staged in the index
 for file in $(git diff --cached --name-only --diff-filter=ACM | grep "\.js$"); do
 
-    # Prevent console.log() or alert statements from being committed.
-    # adapted from jlindley's console check https://gist.github.com/673376
-    grep_bad=$(grep -inR "console\.\|alert(\|debugger" $file)
-    count=$(echo -e "$grep_bad" | grep "[^\s]" | wc -l | awk '{print $1}')
+	# Prevent console.log() or alert statements from being committed.
+	# adapted from jlindley's console check https://gist.github.com/673376
+	grep_bad=$(grep -inR "console\.\|alert(\|debugger" $file)
+	count=$(echo -e "$grep_bad" | grep "[^\s]" | wc -l | awk '{print $1}')
 
-    if [[ "$count" -ge 1 ]]; then
-	echo "[warning] aborting commit" 1>&2
-	echo "[warning] $count config.log/alert found:" 1>&2
-	echo -e "$grep_bad" 1>&2
-	exit 1
-    fi
+	if [[ "$count" -ge 1 ]]; then
+		echo "[warning] aborting commit" 1>&2
+		echo "[warning] $count config.log/alert found:" 1>&2
+		echo -e "$grep_bad" 1>&2
+		exit 1
+	fi
 
-    # push the current file's contents to the jshint service
-    hints=$(curl -s -f -m 2 --form "source=<$file" --form "config=<$options_file" "$jshint_uri")
-    on_error "couldn't connect to $jshint_uri"
-    
-    # if there's at least one line of output from the curl reponse
-    # dump it to stdout for review
-    counts=$(echo -e "$hints" | grep "[^\s]" | wc -l)
-    if [[  "$counts" -gt 0 ]]; then
-	echo
-	echo "$file":
-	echo "$hints"
-    fi
+	# push the current file's contents to the jshint service
+	hints=$(curl -s -f -m 2 --form "source=<$file" --form "config=<$options_file" "$jshint_uri")
+	on_error "couldn't connect to $jshint_uri"
+
+	# if there's at least one line of output from the curl reponse
+	# dump it to stdout for review
+	counts=$(echo -e "$hints" | grep "[^\s]" | wc -l)
+	if [[  "$counts" -gt 0 ]]; then
+		echo
+		echo "$file":
+		echo "$hints"
+	fi
 done
